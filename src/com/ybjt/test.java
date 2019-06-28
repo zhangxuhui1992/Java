1f457a5d5eba6b0c4b5797487e582c3e91e1897d
Administrator <1083606942@qq.com>
2019-06-28 10:49:23 +0800

    second commit

--- src/com/ybjt/test.java
+++ src/com/ybjt/test.java
@@ -6,6 +6,9 @@ package com.ybjt;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World！！！！");
+        for (int i = 0;i<10;i++){
+            System.out.println("123");
+        }
     }
 }
