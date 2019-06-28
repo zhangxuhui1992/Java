c539842bc0c1ad3d48be61100884bca41ad2a2f6
Administrator <1083606942@qq.com>
2019-06-28 11:02:06 +0800

    five commit

---
+++ src/com/ybjt/test/variable.java
@@ -0,0 +1,13 @@
+package com.ybjt.test;
+
+/**
+ * 变量
+ * @author zxh
+ * @create 2019-06-28 11:00
+ */
+public class variable {
+    public static void main(String [] args ){
+        byte a = 127;
+        System.out.println(a);
+    }
+}
