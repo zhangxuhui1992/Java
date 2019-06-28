ba1ea022f68197faeeb251fa6826b16bb0f6cb61
Administrator <1083606942@qq.com>
2019-06-28 10:44:03 +0800

    first commit

---
+++ src/com/ybjt/test.java
@@ -0,0 +1,11 @@
+package com.ybjt;
+
+/**
+ * @author zxh
+ * @create 2019-06-28 10:42
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
