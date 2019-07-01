7a3e51ea23e6ea975dd2de50243fda6416bc37c1
Administrator <1083606942@qq.com>
2019-07-01 09:36:27 +0800

    six commit

--- src/com/ybjt/test/variable.java
+++ src/com/ybjt/test/variable.java
@@ -7,7 +7,14 @@ package com.ybjt.test;
  */
 public class variable {
     public static void main(String [] args ){
-        byte a = 127;
+        //byte表示数的范围-128-127 想要知道某种基本数据类型的位数、能够表示的最大值
+        //和最小值，使用该数据类型的包装类打点调用相应的方法即可。
+        /**
+         * 整数默认为int，浮点型默认为double，声明lang类型的数据加"L"
+         * 声明float类型的数据加"F",
+         */
+        byte a =28;
         System.out.println(a);
+        System.out.println(Byte.MAX_VALUE);
     }
 }
