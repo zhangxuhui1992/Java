a7913102395fa5bac2c91f5b5733bb4b491ef1a2
Administrator <1083606942@qq.com>
2019-07-18 21:22:59 +0800

    10commit

---
+++ src/com/ybjt/test/TeatArray.java
@@ -0,0 +1,15 @@
+package com.ybjt.test;
+
+/**
+ * @author zxh
+ * @create 2019-07-18 21:19
+ * 数组元素有默认值
+ */
+public class TeatArray {
+    public static void main(String args[]){
+        int[] array = new int[10];
+        for(int num:array){
+            System.out.println("num= "+num);
+        }
+    }
+}
