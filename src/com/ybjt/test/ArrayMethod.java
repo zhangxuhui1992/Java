32c200bc4c0a929a23915404c8292e2d34fda06b
Administrator <1083606942@qq.com>
2019-08-07 22:54:24 +0800

    随机数

---
+++ src/com/ybjt/test/ArrayMethod.java
@@ -0,0 +1,18 @@
+package com.ybjt.test;
+
+/**
+ * @author zxh
+ * @create 2019-08-07 21:29
+ * $定义一个int类型的10元素2位数组。并随机赋值。然后求最大值、
+ * 最小值、平均值
+ * [10,99]   (int)(Math.random()*(99-10+1)+10)
+ */
+public class ArrayMethod {
+    public static void main(String[] args) {
+        int [] array = new int [10];
+        for(int i = 0 ;i<array.length;i++){
+            array[i] = (int)(Math.random()*(99-10+1)+10);
+            System.out.print(array[i]+"\t");
+        }
+    }
+}
