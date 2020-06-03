4284e5e2729d0428683debeadf8b37d02d291583
zhangxuhui1992 <1083606942@qq.com>
2020-06-03 17:38:40 +0800

    手撸基础023

---
+++ src/test/javaee/collection/CollectionTest.java
@@ -0,0 +1,22 @@
+package test.javaee.collection;
+
+import java.util.*;
+
+/**
+ * @author zhangxuhui
+ * @email zxh_1633@163.com
+ * @create 2020-06-03 14:43
+ */
+public class CollectionTest {
+    public static void main(String[] args) {
+        /**
+         * list 有序可以重复 ArrayList 底层使用obejct数组
+         * set 无序不可以重复
+         */
+        List list = new ArrayList();
+
+        List lined = new LinkedList();
+
+        Map map = new HashMap();
+    }
+}
---
+++ src/test/javaee/functionInterface/FunctionInterface.java
@@ -0,0 +1,18 @@
+package test.javaee.functionInterface;
+
+/**
+ * @author zhangxuhui
+ * @email zxh_1633@163.com
+ * @create 2020-06-03 14:21
+ * 函数式接口
+ */
+public class FunctionInterface {
+    public static void main(String[] args) {
+    }
+}
+
+
+
+
+
+
