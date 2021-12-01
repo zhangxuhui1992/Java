843147105b903b4bd7797f2a631e2900c06a5cae
zhangxuhui <1083606942@qq.com>
2021-12-01 19:42:16 +0800

    java 再学习01

---
+++ src/newStudy/s01/Study01.java
@@ -0,0 +1,12 @@
+package newStudy.s01;
+
+/**
+ * @Author zhangxuhui
+ * @Date 2021/12/1
+ * @email zxh_1633@163.com
+ */
+public class Study01 {
+    public static void main(String[] args) {
+        System.out.println("我爱中国。。");
+    }
+}
