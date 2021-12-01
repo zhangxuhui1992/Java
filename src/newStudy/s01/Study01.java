e091229755067fdc24a8955e2f98f17eeb040e32
zhangxuhui <1083606942@qq.com>
2021-12-01 22:04:42 +0800

    java 再学习01

--- src/newStudy/s01/Study01.java
+++ src/newStudy/s01/Study01.java
@@ -1,5 +1,7 @@
 package newStudy.s01;
 
+import java.util.Properties;
+
 /**
  * @Author zhangxuhui
  * @Date 2021/12/1
@@ -8,5 +10,8 @@ package newStudy.s01;
 public class Study01 {
     public static void main(String[] args) {
         System.out.println("我爱中国。。");
+        Properties pro= System.getProperties();
+        //打印全部的属性
+        pro.list(System.out);
     }
 }
