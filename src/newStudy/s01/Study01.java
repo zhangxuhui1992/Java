c608e36accfaca8e1c480a9784b32b1981863e46
zhangxuhui <1083606942@qq.com>
2021-12-02 19:14:46 +0800

    java 再学习01

--- src/newStudy/s01/Study01.java
+++ src/newStudy/s01/Study01.java
@@ -6,6 +6,7 @@ import java.util.Properties;
  * @Author zhangxuhui
  * @Date 2021/12/1
  * @email zxh_1633@163.com
+ * public 修饰的类的类名必须与文件名相同
  */
 public class Study01 {
     public static void main(String[] args) {
