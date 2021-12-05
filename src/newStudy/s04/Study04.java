b51c1c99a6abd270f4a2d725ab2f87e047719bde
zhangxuhui <1083606942@qq.com>
2021-12-05 10:30:33 +0800

    java 再学习03 if  循环结构

--- src/newStudy/s04/Study04.java
+++ src/newStudy/s04/Study04.java
@@ -1,5 +1,7 @@
 package newStudy.s04;
 
+import org.junit.Test;
+
 import java.util.Scanner;
 
 /**
@@ -45,6 +47,54 @@ public class Study04 {
         }
 
         getWeak();
+    }
+
+    @Test
+    public void test(){
+        int i = 0 ;
+        while (i < 10){
+            System.out.println("i="+i);
+            i++;
+        }
+    }
+
+    /**
+     * for循环
+     */
+    @Test
+    public void test01(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i="+i);
+        }
+//        while(true){}
+        for(;;){}
+    }
+
+    @Test
+    public void test02(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void test03(){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5-i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+        System.out.println("----------");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
     }
 
     /**
