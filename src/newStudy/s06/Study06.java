0be4eb08aaa50092ee1ee73de3767c5058a943a1
zhangxuhui <1083606942@qq.com>
2021-12-07 22:41:30 +0800

    java 再学习06 数组的定义-加班10点到家

--- src/newStudy/s06/Study06.java
+++ src/newStudy/s06/Study06.java
@@ -9,12 +9,28 @@ import java.util.Arrays;
  * 数组
  */
 public class Study06 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int [] arr = new int[5];//会默认初始化
         int [] arr2 = new int[]{1,2,3,4,5};
         int [] arr3 = {6,7,8,9,10};
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(arr2));
         System.out.println(Arrays.toString(arr3));
+
+        getRomand();
     }
+
+    /**
+     * 利用math工具类输出0-10000之间的随机数
+     */
+    public static void getRomand() throws InterruptedException {
+
+        for(;;){
+            System.out.println((int)(Math.random()*10000)+10000);
+            Thread.sleep(1000);
+        }
+
+    }
+
+
 }
